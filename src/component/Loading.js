ea1f4a53fd786d2f05567ef6e49a9adf2533c007
karan-mishra-sudo <mishrazack69@gmail.com>
2024-09-22 19:58:22 +0530

    Delete src directory

--- src/component/Loading.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import gifimage from './spneer.gif'
-export default function Loading() {
-  return (
-    <div className='container vh-100 d-flex justify-content-center align-items-center p-5'>
-       <img src={gifimage} alt="Example GIF" className='rounded-circle'  />
-    </div>
-  )
-}
